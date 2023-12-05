b747a306b1c1e49e0e8af1af2f06fbe220c885ed
tanx25 <tanx25@mcmaster.ca>
2023-12-05 16:09:12 -0500

    Adding topic selection and update todo list

---
+++ static/js/project_list.js
@@ -0,0 +1,51 @@
+ function Time() {
+            var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', timeZone: 'America/New_York', timeZoneName: 'short' };
+            document.getElementById('CurrentTime').innerText = new Date().toLocaleTimeString('en-US', options);
+        }
+        setInterval(Time, 1000);
+
+
+function StartDate() {
+    selectedStartDate = document.getElementById('startDateChoose').value;
+    document.getElementById('start_date').innerText = 'Start Date: ' + selectedStartDate;
+    calculateWeekDifference();
+}
+var selectedStartDate;
+
+function calculateWeekDifference() {
+    var timeDifference = document.getElementById('timeDifference');
+    if (selectedStartDate) {
+        var startDate = new Date(selectedStartDate);
+        var now = new Date();
+        var differenceInTime = now.getTime() - startDate.getTime();
+        var differenceInDays = differenceInTime / (1000 * 3600 * 24);
+        var differenceInWeeks = Math.ceil(differenceInDays / 7);
+        if (differenceInWeeks > 10) {
+            timeDifference.innerText = "Completed!";
+        } else {
+            timeDifference.innerText = "Week " + differenceInWeeks;
+        }
+    } else {
+        timeDifference.innerText = "";
+    }
+}
+ function scrollWeeks(direction) {
+        var container = document.querySelector('.week-scroll');
+        var scrollAmount = container.offsetWidth / 3;
+        if (direction === 'left') {
+        container.scrollLeft -= scrollAmount;
+    } else {
+        container.scrollLeft += scrollAmount;
+    }
+}
+function confirmTopic() {
+    var topicSelectElement = document.getElementById("topicSelection");
+    var selectedValue = topicSelectElement.options[topicSelectElement.selectedIndex].text;
+    var selectedTopicText = document.getElementById("selectedTopicText");
+
+    if(selectedValue !== "" && selectedValue !== "--Please choose a topic--") {
+        selectedTopicText.textContent = `The topic you chose: ${selectedValue}`;
+    } else {
+        alert("Please select a topic before confirming.");
+    }
+}
